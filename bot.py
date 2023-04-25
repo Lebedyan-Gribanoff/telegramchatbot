9b813efa1194370a1fe5d69af12b79cd943fc4d9
Lebedyan-Gribanoff <118825816+Lebedyan-Gribanoff@users.noreply.github.com>
2023-04-25 12:43:55 +0300

    Update and rename bot_beta.py to bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import sqlite3
+import asyncio
 from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup
@@ -9,7 +10,8 @@ TOKEN = '6034472814:AAGQMRiI97yXXlIlok6yC0K08eCZTSILFf0'
 
 #
 reply_keyboard = [['/new', '/show_all'],
-                  ['/delete', '/delete_all']]
+                  ['/delete', '/delete_all'],
+                  ['/remind', '/stop']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 # создаем дб где будем хранить список задач
@@ -44,10 +46,12 @@ async def start(update, context):
 async def help(update, context):
     user = update.effective_user
     await update.message.reply_text(
-        "/new <name> <time> - создать новую задачу;\n"
+        "/new <name> <time> - создать новую задачу, ремя в формате 2023-04-25 12:00;\n"
         "/show_all - показать список твоих активных задач;\n"
-        "/delete <task_id> - удалить одну из задач по номеру;\n"
-        "/delete_all - удалить все задачи")
+        "/delete <task_id> - удалить одну из ваших задач по номеру;\n"
+        "/delete_all - удалить все задачи;\n"
+        "/remind - бот начнет отправлять напоминания по времени;\n"
+        "/stop - бот не будет вас беспокоить")
 
 
 #
@@ -60,9 +64,10 @@ async def new_task(update, context):
             "Неправильный формат ввода",
         )
         return 0
-    task = msg[0:-1]
-    date = msg[-1]
-    c.execute("INSERT INTO tasks (user_id, task, date) VALUES (?, ?, ?)", (user_id, ' '.join(task), date))
+    task = msg[0:-2]
+    date = msg[-2] + ' ' + msg[-1]
+    print(date)
+    c.execute("INSERT INTO tasks (user_id, task, date) VALUES (?, ?, ?)", (user_id, ' '.join(task), (date)))
     conn.commit()
     await update.message.reply_text(
         'Задача добавлена, это успех!',
@@ -72,11 +77,11 @@ async def new_task(update, context):
 
 #
 def print_rows_by_id(id):
-    # формируем запрос на выборку строк с заданным id
+    # формируем запрос на поиск строк пользователя
     query = f"SELECT * FROM tasks WHERE user_id = {id}"
 
     c.execute(query) # выполняем запрос
-    rows = c.fetchall() # получаем все найденные строки результата
+    rows = c.fetchall() # получаем все найденные строки
 
     if rows:
         return rows
@@ -117,6 +122,7 @@ async def delete_task(update, context):
             f'Хорошо, задача под номером {task_id} удаленa',
         )
     else:
+        #
         await update.message.reply_text(
             f'Задача с номером {task_id} не найдена!',
         )
@@ -127,14 +133,54 @@ async def delete_all(update, context):
     user = update.effective_user
     user_id = update.message.chat_id
     c.execute("DELETE FROM tasks WHERE user_id=?", (user_id,))
+    conn.commit()
     await update.message.reply_text(
         "Успех! Удалено всё то, что может быть удалено",
     )
 
 
-# функция которая будет отправлять напоминания в нужное время
-async def remind():
-    pass
+# функция, которая будет отправлять напоминания в нужное время
+async def remind(update, cotext):
+    user = update.effective_user
+    user_id = update.message.chat_id
+
+    query = f"SELECT * FROM tasks WHERE user_id = {id}"
+    c.execute(query)  # выполняем запрос
+    rows = c.fetchall()  # получаем все найденные строки
+
+    if rows is None:
+        await update.message.reply_text(
+            'Похоже, ты ничего не запланировал!',
+        )
+        return 0
+
+    else:
+        await update.message.reply_text(
+            "Хорошо, теперь я буду отправлять напоминания",
+        )
+
+        while True:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+            print(now)
+
+            c.execute("SELECT * FROM tasks WHERE date=? AND user_id=?", (now, user_id))
+            rows = c.fetchall()
+            if rows is not None:
+                for row in rows:
+                    task = row[2]
+                    await update.message.reply_text(f"Напоминаю: {task}, время:{now}")
+                    c.execute("DELETE FROM tasks WHERE task=? AND user_id=?", (task, user_id))
+                    conn.commit()
+
+            c.execute(query)
+            rows = c.fetchall()
+            if rows is None:
+                await update.message.reply_text(
+                    'Это была последняя задача',
+                )
+                return 0
+
+            await asyncio.sleep(60)
 
 
 def main():
@@ -147,10 +193,12 @@ def main():
     application.add_handler(CommandHandler("show_all", show_tasks))
     application.add_handler(CommandHandler("delete", delete_task))
     application.add_handler(CommandHandler("delete_all", delete_all))
-
+    application.add_handler(CommandHandler("remind", remind))
 
     # Запускаем приложение.
     application.run_polling()
+
+
 
 
 # Запускаем функцию main()
